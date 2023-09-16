1a92f1f3d33e1825c2cc5a5b8a45537503818c3a
chughtaie <hafizchughtai09@gmail.com>
2023-09-16 21:56:16 +0500

    divide added

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
 def add(x, y):
     return x + y
+
 def multiply(a,b): 
     return a*b 
 
 def sub(a,b):
     return a-b
 
+def divide(a,b): 
+    if a==0:
+        return 0
+    else:
+        return a/b
